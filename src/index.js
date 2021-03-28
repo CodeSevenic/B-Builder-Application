a20a1e7c75dab40a5be59a379935a45c510c0a0a
CodeSevenic <sbu.audio.production@gmail.com>
2021-03-28 13:47:19 +0200

    my changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const reduxDevtools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, reduxDevtools);
 
 const app = (
   <Provider store={store}>
