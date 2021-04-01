426fb5cc658f706bb0d32d501268c75b68dcb012
CodeSevenic <sbu.audio.production@gmail.com>
2021-04-01 11:57:42 +0200

    my changes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime);
+  };
+};
+
 export const auth = (email, password, isSignUp) => {
   const API_KEY = 'AIzaSyDpiZadqmqWxkIIsRcN5rYwAQG9uOep2vk';
   return (dispatch) => {
@@ -40,6 +54,7 @@ export const auth = (email, password, isSignUp) => {
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((error) => {
         dispatch(authFail(error.response.data.error));
