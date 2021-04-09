fc0f7d323a18a31d8e18ba0ccb2c9cc14333acd4
CodeSevenic <sbu.audio.production@gmail.com>
2021-04-09 12:12:17 +0200

    my changes

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -36,6 +36,10 @@ const authLogout = (state, action) => {
   });
 };
 
+const setAuthRedirectPath = (state, action) => {
+  return updateObject(state, { authRedirectPath: action.path });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -46,6 +50,8 @@ const reducer = (state = initialState, action) => {
       return authFail(state, action);
     case actionTypes.AUTH_LOGOUT:
       return authLogout(state, action);
+    case actionTypes.SET_AUTH_REDIRECT_PATH:
+      return setAuthRedirectPath(state, action);
     default:
       return state;
   }
